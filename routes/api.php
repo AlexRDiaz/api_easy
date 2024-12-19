14a93962570b7ed81fde1066ed01b20e79901846
DianaC <dcarlosamam@gmail.com>
2024-12-19 11:57:27 -0500

    ...

--- routes/api.php
+++ routes/api.php
@@ -696,7 +696,7 @@ Route::middleware(['cors'])->group(function () {
     Route::post('pedidos-shopify/updatepaymentbyidexternal', [PedidosShopifyAPIController::class, 'updateGestionedPaymentCostDeliveryByIdExternal']);
 
 
-    //test
+    //test..
 
 });
 
