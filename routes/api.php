983efc9a3170939ed322999e8d8b2723ce087cb3
DianaC <dcarlosamam@gmail.com>
2025-02-19 23:02:02 -0500

    ...test17

--- routes/api.php
+++ routes/api.php
@@ -706,8 +706,6 @@ Route::middleware(['cors'])->group(function () {
         Route::post('byprovincia', [CoverageExternalAPIController::class, 'byProvincia']);
     });
 
-    Route::post('routetest', [CoverageExternalAPIController::class, 'generalDataOptimizedv2']);
-
     //...
 });
 
